58484cf1bbcb319497250d2bc58f0cae06cb6dcf
maKizo009 <113452307+maKizo009@users.noreply.github.com>
2023-07-05 23:30:56 -0300

    Estou testando um arquivo EXE para facilitar a instalação do programa. Além disso, o programa agora tem uma janela de diálogo que permite escolher a planilha mais facilmente.

--- sheetMasters.py
+++ sheetMasters.py
@@ -3,105 +3,126 @@ import csv
 import os
 from openpyxl import load_workbook
 from shutil import copyfile
+import tkinter as tk
+from tkinter import filedialog
 
-perguntaDados = input("Adicione o nome da planilha que você quer usar e que esteja na pasta 'Downloads': ")
+janelaUsuario = tk.Tk()
+larguraJanela = 500
+alturaJanela = 300
+janelaUsuario.geometry(f"{larguraJanela}x{alturaJanela}")
 
-caminho_para_downloads = os.path.expanduser("~/Downloads")
-caminho_completo_para_arquivo = os.path.join(caminho_para_downloads, perguntaDados)
+def encontrandoArquivo():
+    selecioneArquivo = filedialog.askopenfilename(initialdir=os.path.expanduser("~/Downloads"), title="Selecione o CSV nessa pasta", filetypes=(("Arquivos CSV", "*.csv"), ("Todos os arquivos", "*.*")))
+    processandoArquivo(selecioneArquivo)
 
+botaoSelecionarArquivo = tk.Button(janelaUsuario, text="Selecionar CSV", command=encontrandoArquivo)
+botaoSelecionarArquivo.pack(pady=10)
 
-#Formatando a planilha e delimitando os dados usando o ";"
-with open(caminho_completo_para_arquivo, "r") as f_entrada:
-    leitor = csv.reader(f_entrada, delimiter=";")
+def processandoArquivo(arquivo):
+    if arquivo:
+        #Usando ; para delimitar as colunas da planilha
+        print("Arquivo selecionado: ", arquivo)
+                        #Criando os boto~ões e atribuindo as funções
 
-    #Pegando o nome da estação
-    primeira_linha = f_entrada.readline().rstrip()
-    print(primeira_linha)
+        #Formatando a planilha e delimitando os dados usando o ";"
+        with open(arquivo, "r") as f_entrada:
+            leitor = csv.reader(f_entrada, delimiter=";")
 
-    #Tranformando os dados da planilha em um Dataframe
-    leitor = pd.DataFrame(leitor)
+            #Pegando o nome da estação
+            primeira_linha = f_entrada.readline().rstrip()
+            print(primeira_linha)
 
-#Ecluindo as partes inúteis da planilha, localizada nas primeiras 11 linhas
-    leitor = leitor.iloc[11:]
+            #Tranformando os dados da planilha em um Dataframe
+            leitor = pd.DataFrame(leitor)
 
-#Renomeando e reordenando as colunas da forma que eu quero
-    leitor = leitor.rename(columns={0: "Data", 1: "Max", 2: "Min", 3: "Precipitação"})
-    leitor = leitor.reindex(columns=["Data", "Min", "Max"])
-#Tranformando os dados de string para float
-    leitor["Max"] = pd.to_numeric(leitor["Max"], errors="coerce")#.astype(float)
-    leitor["Min"] = pd.to_numeric(leitor["Min"], errors="coerce")#.astype(float)
-    
-#Criando um pequeno resumo da estação
-    linha = leitor["Max"].idxmin()
-    colunas = ["Data", "Min", "Max"]
-    resultado = leitor.loc[linha, colunas]
+        #Ecluindo as partes inúteis da planilha, localizada nas primeiras 11 linhas
+            leitor = leitor.iloc[11:]
 
-    linha2 = leitor["Min"].idxmin()
-    colunas2 = ["Data", "Min", "Max"]
-    resultado2 = leitor.loc[linha2, colunas2]
+        #Renomeando e reordenando as colunas da forma que eu quero
+            leitor = leitor.rename(columns={0: "Data", 1: "Max", 2: "Min", 3: "Precipitação"})
+            leitor = leitor.reindex(columns=["Data", "Min", "Max"])
+        #Tranformando os dados de string para float
+            leitor["Max"] = pd.to_numeric(leitor["Max"], errors="coerce")
+            leitor["Min"] = pd.to_numeric(leitor["Min"], errors="coerce")
+            
+        #Criando um pequeno resumo da estação
+            linha = leitor["Max"].idxmin()
+            colunas = ["Data", "Min", "Max"]
+            resultado = leitor.loc[linha, colunas]
 
-    linha3 = leitor["Max"].idxmax()
-    colunas3 = ["Data", "Min", "Max"]
-    resultado3 = leitor.loc[linha3, colunas3]
-    
-    linha4 = leitor["Min"].idxmax()
-    colunas4 = ["Data", "Min", "Max"]
-    resultado4 = leitor.loc[linha4, colunas4]
+            linha2 = leitor["Min"].idxmin()
+            colunas2 = ["Data", "Min", "Max"]
+            resultado2 = leitor.loc[linha2, colunas2]
 
-    leitor["Data"] = pd.to_datetime(leitor["Data"])
+            linha3 = leitor["Max"].idxmax()
+            colunas3 = ["Data", "Min", "Max"]
+            resultado3 = leitor.loc[linha3, colunas3]
+            
+            linha4 = leitor["Min"].idxmax()
+            colunas4 = ["Data", "Min", "Max"]
+            resultado4 = leitor.loc[linha4, colunas4]
 
-    leitor["Dia"] = leitor["Data"].dt.day
-    leitor["Mes"] = leitor["Data"].dt.month
-    leitor["Ano"] = leitor["Data"].dt.year
+            leitor["Data"] = pd.to_datetime(leitor["Data"])
 
-caminhoOriginal = "Modelo para normais climatológicas 1991-2020.xlsx"
-novoCaminho = (primeira_linha[6:]+"_BDMEP.xlsx")
+            leitor["Dia"] = leitor["Data"].dt.day
+            leitor["Mes"] = leitor["Data"].dt.month
+            leitor["Ano"] = leitor["Data"].dt.year
 
-copyfile(caminhoOriginal, novoCaminho)
+        caminhoOriginal = "Modelo para normais climatológicas 1991-2020.xlsx"
+        novoCaminho = (primeira_linha[6:]+"_BDMEP.xlsx")
 
-    #Imprimindo os resultados na tela
-dados1 = ("A estação de {}, registrou sua menor máxima em {}, com {} graus, e a maior em {} com {}.".format(primeira_linha, resultado["Data"], resultado["Max"], resultado3["Data"], resultado3["Max"]))
-dados2 = ("Nas mínimas, a menor foi {} graus em {}, e a maior foi {}, em {}.".format(resultado2["Min"], resultado2["Data"], resultado4["Min"], resultado4["Data"]))
+        copyfile(caminhoOriginal, novoCaminho)
 
-print(dados1, dados2)
-print("Sua planilha está sendo automatizada...")
+            #Imprimindo os resultados na tela
+        dados1 = ("A estação de {}, registrou sua menor máxima em {}, com {} graus, e a maior em {} com {}.".format(primeira_linha, resultado["Data"], resultado["Max"], resultado3["Data"], resultado3["Max"]))
+        dados2 = ("Nas mínimas, a menor foi {} graus em {}, e a maior foi {}, em {}.".format(resultado2["Min"], resultado2["Data"], resultado4["Min"], resultado4["Data"]))
 
-book = load_workbook("Modelo para normais climatológicas 1991-2020.xlsx")
+        print(dados1, dados2)
+        print("Sua planilha está sendo automatizada...")
 
-for ano in leitor["Ano"].dropna().unique():
-    dadosAno = leitor[leitor["Ano"]== ano]
-    organizado = pd.DataFrame()
+        book = load_workbook("Modelo para normais climatológicas 1991-2020.xlsx")
 
-    for mes in range(1, 13):
-        dadosMes = dadosAno[dadosAno["Mes"] == mes]
-        minimas = dadosMes[["Dia", "Min"]].set_index("Dia")
-        maximas = dadosMes[["Dia", "Max"]].set_index("Dia")
-        minimas.columns = [f"Minima{mes}"]
-        maximas.columns = [f"Máxima{mes}"]
-        organizado.index = organizado.index.astype(str)
-        inicio = pd.to_datetime(f'{int(ano)}-{int(mes):02d}-01')
-        fim = inicio + pd.offsets.MonthEnd(0)
-        dias = pd.date_range(inicio, fim)
-        dias = dias.day
-        minimas = minimas.reindex(dias)
-        maximas = maximas.reindex(dias)
-        organizado = organizado.reset_index(drop=True)
-        minimas = minimas.reset_index(drop=True)
-        maximas = maximas.reset_index(drop=True)
-        organizado = pd.concat([organizado, minimas, maximas], axis=1)
+        for ano in leitor["Ano"].dropna().unique():
+            dadosAno = leitor[leitor["Ano"]== ano]
+            organizado = pd.DataFrame()
 
-    sheet_name = str(int(ano))
-    if sheet_name in book.sheetnames:
-        ws = book[sheet_name]
+            for mes in range(1, 13):
+                dadosMes = dadosAno[dadosAno["Mes"] == mes]
+                minimas = dadosMes[["Dia", "Min"]].set_index("Dia")
+                maximas = dadosMes[["Dia", "Max"]].set_index("Dia")
+                minimas.columns = [f"Minima{mes}"]
+                maximas.columns = [f"Máxima{mes}"]
+                organizado.index = organizado.index.astype(str)
+                inicio = pd.to_datetime(f'{int(ano)}-{int(mes):02d}-01')
+                fim = inicio + pd.offsets.MonthEnd(0)
+                dias = pd.date_range(inicio, fim)
+                dias = dias.day
+                minimas = minimas.reindex(dias)
+                maximas = maximas.reindex(dias)
+                organizado = organizado.reset_index(drop=True)
+                minimas = minimas.reset_index(drop=True)
+                maximas = maximas.reset_index(drop=True)
+                organizado = pd.concat([organizado, minimas, maximas], axis=1)
+
+            sheet_name = str(int(ano))
+            if sheet_name in book.sheetnames:
+                ws = book[sheet_name]
+            else:
+                ws = book.create_sheet(sheet_name)
+
+            for col, col_data in enumerate(organizado.values.T, start=2):
+                for row, value in enumerate(col_data, start=3):
+                    if pd.isna(value):
+                        value = "-"
+                    ws.cell(row=row, column=col, value=value)
+
+        book.save(novoCaminho)
+        print("Sua planilha foi formatada automaticamente. Volte sempre!")
+
     else:
-        ws = book.create_sheet(sheet_name)
+        print("Nenhum arquivo foi encontrado")
 
-    for col, col_data in enumerate(organizado.values.T, start=2):
-        for row, value in enumerate(col_data, start=3):
-            if pd.isna(value):
-                value = "-"
-            ws.cell(row=row, column=col, value=value)
+janelaUsuario.mainloop()
 
-book.save(novoCaminho)
-print("Sua planilha já foi formatada automaticamente. Volte sempre!")
+
 
