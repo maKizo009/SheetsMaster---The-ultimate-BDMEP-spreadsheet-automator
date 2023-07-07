cb27bb3e094125076a07cc7dbec8632aaef9ee1e
maKizo009 <113452307+maKizo009@users.noreply.github.com>
2023-07-06 23:24:06 -0300

    Muitos bugs, mas também evoluções no código

--- sheetMasters.py
+++ sheetMasters.py
@@ -5,12 +5,15 @@ from openpyxl import load_workbook
 from shutil import copyfile
 import tkinter as tk
 from tkinter import filedialog
-from tkinter import scrolledtext
 from tkinter import messagebox
+from functools import partial
 
 
 class SheetsMasters:
     def __init__(self) -> None:
+        self.leitor = None
+        self.caminhoPlanilhaDestino = None
+        self.book = None
         self.janelaUsuario = tk.Tk()
         self.larguraJanela = 500
         self.alturaJanela = 300
@@ -19,148 +22,134 @@ class SheetsMasters:
         self.labelArquivo =tk.Label(self.janelaUsuario, text="")
         self.labelArquivo.pack()
         self.labelTexto = tk.Label(self.janelaUsuario, text="")
-        self.mensagemErro = messagebox.showerror("", "")
-        self.nomeDaEstacao = tk.Label(self.janelaUsuario, text=self.primeira_linha)
-        self.nomeDaEstacao.pack()
+        #self.mensagemErro = messagebox.showerror("Nenhum arquivo selecionado", "Por favor, selecione um arquivo com formato de planilha .csv, ou descompacte a pasta .zip que contém o arquivo")
 
-        self.botaoSelecionarArquivo = tk.Button(self.janelaUsuario, text="Selecionar Planilha BDMEP", command=self.encontrandoArquivo)
-        self.botaoSelecionarArquivo.pack()
-        self.botaoOutraPlanilha = tk.Button(self.janelaUsuario, text="Selecione a planilha de destino dos dados", command=self.planilhaDestino)
-        self.botaoNaoPlanilha = tk.Button(self.janelaUsuario, text="Não tenho planilha", command=naoTenhoPlanilha)
+        self.botaoPlanilhaDestino = tk.Button(self.janelaUsuario, text="Selecionar Planilha BDMEP", command=self.encontrandoArquivo)
+        self.botaoPlanilhaDestino.pack()
+        self.botaoPlanilhaDestino = tk.Button(self.janelaUsuario, text="Selecione a planilha de destino dos dados", command=self.planilhaDestino)
+        self.botaoPlanilhaDestino.pack()
+        self.labelCaminhoPlanilha = tk.Label(self.janelaUsuario, text="")
+        self.labelCaminhoPlanilha.pack()
+        self.botaoNaoPlanilha = tk.Button(self.janelaUsuario, text="Não tenho planilha", command=partial(self.naoTenhoPlanilha, self.leitor))
         self.botaoNaoPlanilha.pack()
         self.janelaUsuario.mainloop()
 
 
     def encontrandoArquivo(self):
         while True:
-            self.botaoSelecionarArquivo = filedialog.askopenfilename(initialdir=os.path.expanduser("~/Downloads"), title="Selecione o CSV nessa pasta", filetypes=(("Arquivos CSV", "*.csv"), ("Todos os arquivos", "*.*")))
-            if self.botaoSelecionarArquivo.endswith(".zip"):
+            self.botaoPlanilhaDestino = filedialog.askopenfilename(initialdir=os.path.expanduser("~/Downloads"), title="Selecione o CSV nessa pasta", filetypes=(("Arquivos CSV", "*.csv"), ("Todos os arquivos", "*.*")))
+            if self.botaoPlanilhaDestino.endswith(".zip"):
                 self.mensagemErro = ("Formato não aceito", "Selecione uma planilha com formato .csv. Caso tenha baixado o arquivo .zip, descompacte a pasta e tente novamente.")
             else:
                 break
-        self.processandoArquivo(self.botaoSelecionarArquivo)
+        self.processandoArquivo(self.botaoPlanilhaDestino)
 
 
-    def processandoArquivo(self, botaoSelecionarArquivo):
-        if  botaoSelecionarArquivo:
-            #Usando ; para delimitar as colunas da planilha
-            self.labelArquivo = ("Arquivo selecionado: ", botaoSelecionarArquivo)
-            #Criando os boto~ões e atribuindo as funções
+    def processandoArquivo(self, botaoPlanilhaDestino):
+        #if  botaoPlanilhaDestino:
+    
+            self.labelArquivo = ("Arquivo selecionado: ", botaoPlanilhaDestino)
 
-            #Formatando a planilha e delimitando os dados usando o ";"
-            with open(botaoSelecionarArquivo, "r") as f_entrada:
+            with open(botaoPlanilhaDestino, "r") as f_entrada:
                 leitor = csv.reader(f_entrada, delimiter=";")
-
-                #Pegando o nome da estação
                 primeira_linha = f_entrada.readline().rstrip()
+                nomeDaEstacao = tk.Label(self.janelaUsuario, text=primeira_linha)
+                nomeDaEstacao.pack()
 
-                #Tranformando os dados da planilha em um Dataframe
-                leitor = pd.DataFrame(leitor)
+                self.leitor = pd.DataFrame(leitor)
+                self.leitor = self.leitor.iloc[11:]
+                self.leitor = self.leitor.rename(columns={0: "Data", 1: "Max", 2: "Min", 3: "Precipitação"})
+                self.leitor = self.leitor.reindex(columns=["Data", "Min", "Max"])
 
-            #Ecluindo as partes inúteis da planilha, localizada nas primeiras 11 linhas
-                leitor = leitor.iloc[11:]
-
-            #Renomeando e reordenando as colunas da forma que eu quero
-                leitor = leitor.rename(columns={0: "Data", 1: "Max", 2: "Min", 3: "Precipitação"})
-                leitor = leitor.reindex(columns=["Data", "Min", "Max"])
-            #Tranformando os dados de string para float
-                leitor["Max"] = pd.to_numeric(leitor["Max"], errors="coerce")
-                leitor["Min"] = pd.to_numeric(leitor["Min"], errors="coerce")
+                self.leitor["Max"] = pd.to_numeric(self.leitor["Max"], errors="coerce")
+                self.leitor["Min"] = pd.to_numeric(self.leitor["Min"], errors="coerce")
                 
-            #Criando um pequeno resumo da estação
-                linha = leitor["Max"].idxmin()
+                linha = self.leitor["Max"].idxmin()
                 colunas = ["Data", "Min", "Max"]
-                resultado = leitor.loc[linha, colunas]
+                resultado = self.leitor.loc[linha, colunas]
 
-                linha2 = leitor["Min"].idxmin()
+                linha2 = self.leitor["Min"].idxmin()
                 colunas2 = ["Data", "Min", "Max"]
-                resultado2 = leitor.loc[linha2, colunas2]
+                resultado2 = self.leitor.loc[linha2, colunas2]
 
-                linha3 = leitor["Max"].idxmax()
+                linha3 = self.leitor["Max"].idxmax()
                 colunas3 = ["Data", "Min", "Max"]
-                resultado3 = leitor.loc[linha3, colunas3]
+                resultado3 = self.leitor.loc[linha3, colunas3]
                 
-                linha4 = leitor["Min"].idxmax()
+                linha4 = self.leitor["Min"].idxmax()
                 colunas4 = ["Data", "Min", "Max"]
-                resultado4 = leitor.loc[linha4, colunas4]
+                resultado4 = self.leitor.loc[linha4, colunas4]
                 
-                            #Imprimindo os resultados na tela
                 dados1 = ("A estação de {}, registrou sua menor máxima em {}, com {} graus, e a maior em {} com {}.".format(primeira_linha, resultado["Data"], resultado["Max"], resultado3["Data"], resultado3["Max"]))
                 dados2 = ("Nas mínimas, a menor foi {} graus em {}, e a maior foi {}, em {}.".format(resultado2["Min"], resultado2["Data"], resultado4["Min"], resultado4["Data"]))
 
+                self.leitor["Data"] = pd.to_datetime(self.leitor["Data"])
 
-                leitor["Data"] = pd.to_datetime(leitor["Data"])
-
-                leitor["Dia"] = leitor["Data"].dt.day
-                leitor["Mes"] = leitor["Data"].dt.month
-                leitor["Ano"] = leitor["Data"].dt.year
+                self.leitor["Dia"] = self.leitor["Data"].dt.day
+                self.leitor["Mes"] = self.leitor["Data"].dt.month
+                self.leitor["Ano"] = self.leitor["Data"].dt.year
 
                 self.labelTexto = (dados1)
                 self.labelTexto = (dados2)
-        else:
-            messagebox.showerror("Nenhum arquivo selecionado", "Por favor, selecione um arquivo com formato de planilha .csv, ou descompacte a pasta .zip que contém o arquivo")
+            """  else:
+            self.mensagemErro """
 
 
-    def processandoDados(self, leitor):
-         book = load_workbook("")
-         for ano in leitor["Ano"].dropna().unique():
-                    dadosAno = leitor[leitor["Ano"]== ano]
-                    organizado = pd.DataFrame()
+    def processandoDados(self, primeira_linha, leitor, book):
+         for ano in self.leitor["Ano"].dropna().unique():
+                dadosAno = self.leitor[self.leitor["Ano"]== ano]
+                organizado = pd.DataFrame()
 
-                    for mes in range(1, 13):
-                        dadosMes = dadosAno[dadosAno["Mes"] == mes]
-                        minimas = dadosMes[["Dia", "Min"]].set_index("Dia")
-                        maximas = dadosMes[["Dia", "Max"]].set_index("Dia")
-                        minimas.columns = [f"Minima{mes}"]
-                        maximas.columns = [f"Máxima{mes}"]
-                        organizado.index = organizado.index.astype(str)
-                        inicio = pd.to_datetime(f'{int(ano)}-{int(mes):02d}-01')
-                        fim = inicio + pd.offsets.MonthEnd(0)
-                        dias = pd.date_range(inicio, fim)
-                        dias = dias.day
-                        minimas = minimas.reindex(dias)
-                        maximas = maximas.reindex(dias)
-                        organizado = organizado.reset_index(drop=True)
-                        minimas = minimas.reset_index(drop=True)
-                        maximas = maximas.reset_index(drop=True)
-                        organizado = pd.concat([organizado, minimas, maximas], axis=1)
+                for mes in range(1, 13):
+                    dadosMes = dadosAno[dadosAno["Mes"] == mes]
+                    minimas = dadosMes[["Dia", "Min"]].set_index("Dia")
+                    maximas = dadosMes[["Dia", "Max"]].set_index("Dia")
+                    minimas.columns = [f"Minima{mes}"]
+                    maximas.columns = [f"Máxima{mes}"]
+                    organizado.index = organizado.index.astype(str)
+                    inicio = pd.to_datetime(f'{int(ano)}-{int(mes):02d}-01')
+                    fim = inicio + pd.offsets.MonthEnd(0)
+                    dias = pd.date_range(inicio, fim)
+                    dias = dias.day
+                    minimas = minimas.reindex(dias)
+                    maximas = maximas.reindex(dias)
+                    organizado = organizado.reset_index(drop=True)
+                    minimas = minimas.reset_index(drop=True)
+                    maximas = maximas.reset_index(drop=True)
+                    organizado = pd.concat([organizado, minimas, maximas], axis=1)
 
-                    sheet_name = str(int(ano))
-                    if sheet_name in book.sheetnames:
-                            ws = book[sheet_name]
-                    else:
-                        ws = book.create_sheet(sheet_name)
+                sheet_name = str(int(ano))
+                if self.caminhoPlanilhaDestino and self.book:
+                    self.book.save(self.caminhoPlanilhaDestino)
+                else:
+                    ws = self.book.create_sheet(sheet_name)
 
-                    for col, col_data in enumerate(organizado.values.T, start=2):
-                        for row, value in enumerate(col_data, start=3):
-                            if pd.isna(value):
-                                value = "-"
-                            ws.cell(row=row, column=col, value=value)
+                for col, col_data in enumerate(organizado.values.T, start=2):
+                    for row, value in enumerate(col_data, start=3):
+                        if pd.isna(value):
+                            value = "-"
+                        ws.cell(row=row, column=col, value=value)
 
-                    book.save(self.planilhaDestino)
+                self.book.save(self.planilhaDestino)
 
                 
-    def planilhaDestino(self, leitor):
+    def planilhaDestino(self):
             
-        self.planilhaDestino = filedialog.askopenfilename(initialdir=os.path.expanduser("/~Downloads"), filetypes=(("Arquivos XLSX", "*.xlsx"), ("Todos os arquivos", "*.*")))
+        self.caminhoPlanilhaDestino = filedialog.askopenfilename(initialdir=os.path.expanduser("/~Downloads"), filetypes=(("Arquivos XLSX", "*.xlsx"), ("Todos os arquivos", "*.*")))
 
-        if self.planilhaDestino:
-                self.labelTexto = ("Planilha encontrada: ", {self.planilhaDestino})
-                book = load_workbook(self.planilhaDestino)
-
-                self.planilhaDestino.pack(pady=10)
+        if self.caminhoPlanilhaDestino:
+            self.labelCaminhoPlanilha.config(text=f"Planilha encontrada: {self.caminhoPlanilhaDestino}")
+            self.book = load_workbook(self.caminhoPlanilhaDestino)
         else:
             pass
 
 
-    def naoTenhoPlanilha(self, primeira_linha, leitor):
-        self.planilhaDestino.pack(pady=10)
+    def naoTenhoPlanilha(self, leitor):
         caminhoOriginal = "Modelo para normais climatológicas 1991-2020.xlsx"
-        novoCaminho = (primeira_linha[6:]+"_BDMEP.xlsx")
+        novoCaminho = "planilhaTeste.xlsx"
+        #(primeira_linha[6:]+"_BDMEP.xlsx")
 
         copyfile(caminhoOriginal, novoCaminho)
-
-        book = load_workbook("Modelo para normais climatológicas 1991-2020.xlsx")
-        
-        self.processandoDados(leitor, novoCaminho)
-SheetsMasters = ()
+        self.book = load_workbook("Modelo para normais climatológicas 1991-2020.xlsx")
+        self.processandoDados(self.leitor, novoCaminho, self.book)
+SheetsMasters()
